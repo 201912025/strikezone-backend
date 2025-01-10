e1da10c294ab58d7edfc410de6abf0f9820e4b2e
201912025 <michi2012@naver.com>
2025-01-10 13:06:17 +0900

    feat : 기본 개발환경 세팅

---
+++ src/main/java/com/strikezone/strikezone_backend/StrikezoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.strikezone.strikezone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrikezoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrikezoneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strikezone/strikezone_backend/StrikezoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strikezone.strikezone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrikezoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
