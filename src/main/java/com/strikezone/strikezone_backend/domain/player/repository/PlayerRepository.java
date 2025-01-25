2dcd36adad9a1d11946425e36d1d7d5bf0d7e227
201912025 <michi2012@naver.com>
2025-01-25 19:34:42 +0900

    PlayerRepository 페치 조인 변경

--- src/main/java/com/strikezone/strikezone_backend/domain/player/repository/PlayerRepository.java
+++ src/main/java/com/strikezone/strikezone_backend/domain/player/repository/PlayerRepository.java
@@ -3,12 +3,13 @@ package com.strikezone.strikezone_backend.domain.player.repository;
 import com.strikezone.strikezone_backend.domain.player.entity.Player;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
-    @EntityGraph(attributePaths = "team")
+    @Query("SELECT p FROM Player p JOIN FETCH p.team WHERE p.id = :playerId")
     Optional<Player> findByIdWithTeam(Long playerId);
 
 }
