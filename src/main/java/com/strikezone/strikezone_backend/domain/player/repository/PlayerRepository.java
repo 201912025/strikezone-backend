ce4d10260326305d8fca3d2e0a7336c0d97d7497
201912025 <michi2012@naver.com>
2025-01-26 15:24:11 +0900

    feat: 팀 전체 조회 쿼리 메서드 구현

--- src/main/java/com/strikezone/strikezone_backend/domain/player/repository/PlayerRepository.java
+++ src/main/java/com/strikezone/strikezone_backend/domain/player/repository/PlayerRepository.java
@@ -1,15 +1,18 @@
 package com.strikezone.strikezone_backend.domain.player.repository;
 
 import com.strikezone.strikezone_backend.domain.player.entity.Player;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     @Query("SELECT p FROM Player p JOIN FETCH p.team WHERE p.id = :playerId")
     Optional<Player> findByIdWithTeam(Long playerId);
+
+    @Query("SELECT p FROM Player p JOIN FETCH p.team WHERE p.id = :playerId")
+    List<Player> findPlayersWithTeamById(Long playerId);
 
 }
