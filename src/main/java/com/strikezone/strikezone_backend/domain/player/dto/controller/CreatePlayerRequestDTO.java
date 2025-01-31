849fd337819084ddd056f411d32468b0cb424e2e
201912025 <michi2012@naver.com>
2025-01-31 19:27:49 +0900

    feat: dto 검증기능 추가

--- src/main/java/com/strikezone/strikezone_backend/domain/player/dto/controller/CreatePlayerRequestDTO.java
+++ src/main/java/com/strikezone/strikezone_backend/domain/player/dto/controller/CreatePlayerRequestDTO.java
@@ -1,14 +1,25 @@
 package com.strikezone.strikezone_backend.domain.player.dto.controller;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class CreatePlayerRequestDTO {
 
+    @NotBlank(message = "이름은 필수 항목입니다.")
     private String name;
+
+    @NotBlank(message = "팀 이름은 필수 항목입니다.")
     private String teamName;
+
+    @NotBlank(message = "포지션은 필수 항목입니다.")
     private String position;
+
+    @Min(value = 1, message = "등번호는 1 이상이어야 합니다.")
+    @Max(value = 99, message = "등번호는 99 이하여야 합니다.")
     private int number;
 
     @Builder
